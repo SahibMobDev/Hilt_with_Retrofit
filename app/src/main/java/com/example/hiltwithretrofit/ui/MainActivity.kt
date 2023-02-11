d749021b03b57d0895f246ff948c7a542ce18190
Sahib <sahibmobdev@gmail.com>
2023-02-11 19:16:25 +0400

    update

--- app/src/main/java/com/example/hiltwithretrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/hiltwithretrofit/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hiltwithretrofit
+package com.example.hiltwithretrofit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hiltwithretrofit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/hiltwithretrofit/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.hiltwithretrofit.utils
+
+object Constants {
+
+    const val API_KEY = "b90c4dfa229a06303d6390561c4f39cb"
+    const val BASEURL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASEURL = "https://image.tmdb.org/t/p/w342"
+
+}
